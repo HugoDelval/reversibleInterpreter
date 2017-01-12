482049cf7a34b32d056cfdc0ec2238cbcbfcd522
HugoDelval <hugodelval@gmail.com>
2017-01-12 13:55:22 +0000

    init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Reversible Interpreter
+
+Simple interpreter with debugger and Ctr-Z ability
+
+# Features
+
+ (a) describe how to use the final interpreter, and 
+
+ (b) for each of the numbered sections of the project there should be a section of the readme describing how much of the section you have completed.
---
+++ app/Main.hs
@@ -0,0 +1,101 @@
+{-# Language MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-} 
+module Main where
+
+import Prelude hiding (lookup, print)
+import qualified Data.Map as Map
+import Data.Maybe
+import qualified System.IO as System
+import Control.Monad.Identity
+import Control.Monad.Except
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.Writer
+import Lib
+
+main :: IO ()
+main = someFunc
+
+
+
+
+{-------------------------------------------------------------------}
+{- The pure expression language                                    -}
+{-------------------------------------------------------------------}
+
+data Val = I Int | B Bool
+           deriving (Eq, Show)
+
+data Expr = Const Val
+     | Add Expr Expr | Sub Expr Expr  | Mul Expr Expr | Div Expr Expr
+     | And Expr Expr | Or Expr Expr | Not Expr 
+     | Eq Expr Expr | Gt Expr Expr | Lt Expr Expr
+     | Var String
+   deriving (Eq, Show)
+
+type Name = String 
+type Env = Map.Map Name Val
+
+lookup k t = case Map.lookup k t of
+               Just x -> return x
+               Nothing -> fail ("Unknown variable "++k)
+
+{-- Monadic style expression evaluator, 
+ -- with error handling and Reader monad instance to carry dictionary
+ --}
+
+type Eval a = ReaderT Env (ExceptT String Identity) a 
+runEval env ex = runIdentity ( runExceptT ( runReaderT ex env) )
+
+evali op e0 e1 = do e0' <- eval e0
+                    e1' <- eval e1
+                    case (e0', e1') of
+                         (I i0, I i1) -> return $ I (i0 `op` i1)
+                         _            -> fail "type error in arithmetic expression"
+
+evalb op e0 e1 = do e0' <- eval e0
+                    e1' <- eval e1
+                    case (e0', e1') of
+                         (B i0, B i1) -> return $ B (i0 `op` i1)
+                         _            -> fail "type error in boolean expression"
+
+evalib op e0 e1 = do e0' <- eval e0
+                     e1' <- eval e1
+                     case (e0', e1') of
+                          (I i0, I i1) -> return $ B (i0 `op` i1)
+                          _            -> fail "type error in arithmetic expression"
+
+eval :: Expr -> Eval Val
+eval (Const v) = return v
+eval (Add e0 e1) = do evali (+) e0 e1
+eval (Sub e0 e1) = do evali (-) e0 e1
+eval (Mul e0 e1) = do evali (*) e0 e1
+eval (Div e0 e1) = do evali div e0 e1
+
+eval (And e0 e1) = do evalb (&&) e0 e1
+eval (Or e0 e1) = do evalb (||) e0 e1
+
+eval (Not e0  ) = do evalb (const not) e0 (Const (B True)) 
+                       where not2 a _ = not a -- hack, hack
+
+eval (Eq e0 e1) = do evalib (==) e0 e1
+eval (Gt e0 e1) = do evalib (>) e0 e1
+eval (Lt e0 e1) = do evalib (<) e0 e1
+                        
+eval (Var s) = do env <- ask
+                  lookup s env
+
+
+{-------------------------------------------------------------------}
+{- The statement language                                          -}
+
+
+data Statement = Assign String Expr
+               | If Expr Statement Statement
+               | While Expr Statement
+               | Print Expr
+               | Seq Statement Statement
+               | Try Statement Statement
+               | Pass                    
+      deriving (Eq, Show)
+
+
